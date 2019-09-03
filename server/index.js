5198bdf2d6d765b52ba066ab5b76e83550017069
Jou-Valencia, Juan Pablo <jjouvalencia@deloitte.co.uk>
2019-09-03 15:28:26 +0100

    nodemon, google auth restructure

--- server/index.js
+++ server/index.js
@@ -1,14 +1,7 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
 const app = express();
+require('./services/passport');
 
-passport.use(new GoogleStrategy());
-
-// app.get('/', (req, res) => {
-//   res.send({ hi: 'mustard' });
-// });
-
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
