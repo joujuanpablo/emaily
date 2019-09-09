5025f41144e96eeb53c2270b767b622e8264fd9f
Jou-Valencia, Juan Pablo <jjouvalencia@deloitte.co.uk>
2019-09-09 13:43:23 -0400

    finish up auth flow and add env configs

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,5 +8,16 @@ module.exports = app => {
     }),
   );
 
+  // these app.get are middlewares than intercept requests before they reach the server
+
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,24 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users'); //grab this schema from "mongoose" (one argument).
+// Note that the User object is our model class (mongoose) which translates to a "collection" instance
+// in MongoDB
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //id for the mongo record (user model ID) and the app as a whole, not the google id. This is because they could be signing in with linkedIn or instagram etc. This id unifies
+  console.log('serialize is done');
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    //findById takes the serialized id and turns it into mongoose id? No at this point it has been deserialized into the mongoose ID and now it's retrieving the record
+    done(null, user);
+    console.log('deserialize is done', id, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +28,24 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile token', profile);
+      console.log('google sends google ID', profile.id);
+      new User({ googleId: profile.id }).save();
+
+      User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (existingUser) {
+            console.log('passport found that the user already exists');
+            done(null, existingUser);
+          } else {
+            console.log(
+              'passport did not find matching user, new user please!',
+            );
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        })
+        .catch(error => console.log('ERROR', error));
     },
   ),
 );
