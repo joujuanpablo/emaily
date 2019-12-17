3316e2e5d141e8433ebe3beaf2c343cbaf2829b7
Jou-Valencia, Juan Pablo <jjouvalencia@deloitte.co.uk>
2019-12-17 14:16:25 -0500

    email validation

--- server/client/src/components/surveys/SurveyForm.jsx
+++ server/client/src/components/surveys/SurveyForm.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   // All caps means that we don't want this to change programmatically
   {
     label: 'Survey Title',
     name: 'title',
+    // noValueError: 'You must provide a survey title'  ...if you wanted custom error messages.
   },
   {
     label: 'Subject Line',
@@ -28,7 +30,7 @@ class SurveyForm extends React.Component {
     return (
       <div>
         {FIELDS.map((field, index) => (
-          <Field {...field} key={index} type='text' component={SurveyField} />
+          <Field {...field} key={index} type='text' component={SurveyField} /> // Survey field will receive all the props given to  Field (its parent)
         ))}
       </div>
     );
@@ -58,11 +60,14 @@ class SurveyForm extends React.Component {
 
 function validate(values) {
   const errors = {};
+  errors.emails = validateEmails(values.emails || ''); //first time it will be empty
+
   FIELDS.forEach(({ name }) => {
     if (!values[name]) {
       errors[name] = `You must provide a value`;
     }
   });
+
   return errors;
 }
 
@@ -70,3 +75,5 @@ export default reduxForm({
   form: 'surveyForm',
   validate,
 })(SurveyForm);
+
+// redux form acts very similarly to connect.
